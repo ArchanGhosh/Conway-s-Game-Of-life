70c21d862cf9468c767ed7ca49757365aa22560c
Archan Ghosh <gharchan@gmail.com>
2020-10-20 23:59:46 +0530

    Add files via upload

--- game.js
+++ game.js
@@ -1,7 +1,7 @@
 const canvas = document.getElementById("canvas");
 const context = canvas.getContext("2d");
-const size = 4;
-const scale = 4;
+const size = 1;
+const scale = 1;
 const resolution = size / scale;
 
 let cells;
@@ -16,12 +16,12 @@ function canv() {
 }
 function draw_on_position(canvas, event){
     var rect = canvas.getBoundingClientRect();
-    var x = event.clientX - rect.left - 25;
-    var y = event.clientY - rect.top - 65;
+    var x = Math.floor(event.clientX - rect.left);
+    var y = Math.floor(event.clientY - rect.top);
     console.log("Coordinate x: " + x,  
                         "Coordinate y: " + y);
     context.fillStyle = 'black';
-    context.fillRect(x, y, 2, 2);    
+    context.fillRect(x, y, 8, 8);    
 }
 
 canvas.addEventListener("mousedown", function(e) 
@@ -29,7 +29,9 @@ canvas.addEventListener("mousedown", function(e)
             draw_on_position(canvas, e); 
         }); 
 
+function draw_cells(){
 
+}
 
 
 
